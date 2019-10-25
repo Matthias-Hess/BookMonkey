1234915f43b0242f11d59e475e648e8432f89297
Matthias Hess <mat.hess@bluewin.ch>
2019-10-25 05:20:32 +0200

    kapitel 9 fertig

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import {Book} from '../shared/book';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {BookStoreService} from '../shared/book-store.service'
 
 @Component({
@@ -12,15 +12,22 @@ export class BookDetailsComponent implements OnInit {
   book: Book;
   constructor(
     private bs: BookStoreService,
+    private router: Router,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     const params = this.route.snapshot.paramMap;
-    this.book = this.bs.getSingle(params.get('isbn'));
+    this.bs.getSingle(params.get('isbn')).subscribe(res => this.book = res);
   }
 
   getRating(num: number) {
     return new Array(num);
+  }
+
+  removeBook() {
+    if (confirm("Buch wirklich löschen?")){
+      this.bs.remove(this.book.isbn).subscribe(res => this.router.navigate(['../'], {relativeTo: this.route}));
+    }
   }
 }
